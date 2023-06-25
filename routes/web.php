16f7fcc96a7a7fd25c871d8dbba266c464eddfd3
martingeorgiev99 <geor0020@hz.nl>
2023-06-25 04:58:29 +0200

    auth fix

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('foos.index');
-})->middleware(['auth', 'verified']);
+    return redirect('foos');
+});
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return redirect()->route('foos.index');
+})->middleware(['auth', 'verified'])->name('foos');
+
+Route::resource('foos', \App\Http\Controllers\FooController::class)->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
