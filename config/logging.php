59c752680e30e299740bb4a1668ef74905172830
Martin Georgiev <137400929+martingeorgiev99@users.noreply.github.com>
2024-06-30 19:31:41 +0200

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -59,10 +59,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-            'replace_placeholders' => true,
+            'driver' => 'errorlog',
+            'level' => 'debug',
         ],
 
         'daily' => [
